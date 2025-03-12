422912ca9810e10b41e309ede252c530a27c6a1e
srikanth <srikanth.thedev@gmail.com>
2025-03-12 05:30:06 +0000

    Add shortenUrl action to save url to the db

---
+++ src/actions/url.ts
@@ -0,0 +1,75 @@
+'use server';
+
+import { nanoid } from 'nanoid';
+import { urlSchema } from '@/lib/schemas';
+import { ApiResponse } from '@/lib/types';
+import { ensureHttps } from '@/lib/utils';
+import { prisma } from '@/lib/prisma-client';
+import { ATTEMPTS_LIMIT, BASE_URL } from '@/lib/constants';
+import { revalidatePath } from 'next/cache';
+
+export async function shortenUrl(
+  formData: FormData,
+  attempts = 0
+): Promise<
+  ApiResponse<{
+    shortUrl: string;
+  }>
+> {
+  //handling the edge case nanoid creating duplicates(unlikely) and preventing stack overflow
+  if (attempts > ATTEMPTS_LIMIT) {
+    return {
+      success: false,
+      error: 'Failed to shorten URL, please try again.'
+    };
+  }
+
+  try {
+    const url = formData.get('url') as string;
+
+    const validatedData = urlSchema.safeParse({
+      url
+    });
+    if (!validatedData.success) {
+      return {
+        success: false,
+        error:
+          validatedData.error.flatten().fieldErrors.url?.[0] || 'Invalid URL'
+      };
+    }
+
+    const originalUrl = ensureHttps(validatedData.data.url);
+    const shortCode = nanoid(8);
+
+    const existingCode = await prisma.url.findUnique({
+      where: {
+        shortCode
+      }
+    });
+    //if the code exists, recursively call the function to generate a new code
+    if (existingCode) {
+      return await shortenUrl(formData, attempts + 1);
+    }
+
+    await prisma.url.create({
+      data: {
+        originalUrl,
+        shortCode
+      }
+    });
+
+    revalidatePath('/');
+
+    return {
+      success: true,
+      data: {
+        shortUrl: `${BASE_URL}/sl/${shortCode}`
+      }
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: 'Failed to shorten URL, please try again.'
+    };
+  }
+}
