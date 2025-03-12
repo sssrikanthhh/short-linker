eeeca6b682d653406745c49045609c6a3b8bd8bf
srikanth <srikanth.thedev@gmail.com>
2025-03-12 05:28:22 +0000

    Modify prisma-client to use single prima client instance

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query', 'error', 'warn', 'info']
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
