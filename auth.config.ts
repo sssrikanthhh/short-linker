3dce91821256793a91b6c39c2385842cde02d192
srikanth <srikanth.thedev@gmail.com>
2025-03-17 13:48:35 +0000

    Add auth config

---
+++ auth.config.ts
@@ -0,0 +1,140 @@
+import { DefaultSession, NextAuthConfig } from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
+import { JWT } from 'next-auth/jwt';
+import {
+  SECRET,
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
+} from '@/lib/constants';
+import { loginSchema } from '@/lib/schemas';
+import { prisma } from '@/lib/prisma-client';
+import { compareValue } from '@/lib/bcrypt';
+
+//extend the types to include role property
+declare module 'next-auth' {
+  interface User {
+    role?: 'USER' | 'ADMIN';
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      role?: 'USER' | 'ADMIN';
+    } & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: 'USER' | 'ADMIN';
+  }
+}
+
+export const authConfig: NextAuthConfig = {
+  secret: SECRET,
+  pages: {
+    signIn: '/login',
+    signOut: '/logout',
+    error: '/login',
+    verifyRequest: '/verify-request',
+    newUser: '/register'
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnAdmin = nextUrl.pathname.startsWith('/admin');
+
+      if (isOnAdmin) {
+        return isLoggedIn && auth?.user?.role === 'ADMIN';
+      } else if (isOnDashboard) {
+        return isLoggedIn;
+      } else if (isLoggedIn) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.image = user.image;
+        token.role = user.role;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as 'USER' | 'ADMIN';
+      }
+      return session;
+    }
+  },
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET
+    }),
+    Google({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET
+    }),
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com'
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: '********'
+        }
+      },
+      async authorize(credentials) {
+        const validatedCredentials = loginSchema.safeParse(credentials);
+
+        if (!validatedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = validatedCredentials.data;
+
+        const lowerCaseEmail = email.toLowerCase();
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email: lowerCaseEmail
+          }
+        });
+
+        if (!user) {
+          return null;
+        }
+        const isPasswordValid = await compareValue(
+          password,
+          user?.password || ''
+        );
+        if (!isPasswordValid) {
+          return null;
+        }
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+          image: user.image
+        };
+      }
+    })
+  ]
+};
