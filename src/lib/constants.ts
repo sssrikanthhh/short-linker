1a02ff10456e488838b1e3e1d73ad3e18e9eac22
srikanth <srikanth.thedev@gmail.com>
2025-03-17 13:48:08 +0000

    Add getEnv method to validate and get the env variables

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,16 @@
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+const getEnv = (key: string, defaultValue?: string) => {
+  const value = process.env[key] || defaultValue;
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+};
+
+export const BASE_URL = getEnv('NEXT_PUBLIC_BASE_URL', 'http://localhost:3000');
+export const SECRET = getEnv('SECRET');
+export const GITHUB_CLIENT_ID = getEnv('GITHUB_CLIENT_ID');
+export const GITHUB_CLIENT_SECRET = getEnv('GITHUB_CLIENT_SECRET');
+export const GOOGLE_CLIENT_ID = getEnv('GOOGLE_CLIENT_ID');
+export const GOOGLE_CLIENT_SECRET = getEnv('GOOGLE_CLIENT_SECRET');
 
 export const ATTEMPTS_LIMIT = 5;
