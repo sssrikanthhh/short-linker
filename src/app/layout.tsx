dc532df26eb92fe910127741b14c750d9234d792
srikanth <srikanth.thedev@gmail.com>
2025-03-18 10:36:48 +0000

    Add toast container to tha root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next";
+import "./globals.css";
+import ToastProvider from "@/components/providers/toast-provider";
 
 export const metadata: Metadata = {
-  title: 'Short Linker',
+  title: "Short Linker",
   description:
-    'Shorten links in seconds with Short Linker, security provided by the power of AI.'
+    "Shorten links in seconds with Short Linker, security provided by the power of AI.",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
+        <ToastProvider />
         <main>{children}</main>
       </body>
     </html>
