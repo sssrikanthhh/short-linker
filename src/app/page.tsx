5e1f5eaf027db43a896ba2a9baab48ab810f62f5
srikanth <srikanth.thedev@gmail.com>
2025-03-21 14:37:55 +0000

    Add auth check to redirect user if they are authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import { Button } from '@/components/ui/button';
-import UrlForm from '@/components/url/url-form';
+"use client";
+
+import UrlForm from "@/components/url/url-form";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session?.user) {
+    redirect("/dashboard");
+  }
   return (
-    <section className='flex flex-1 flex-col items-center justify-center p-6 md:p-24'>
-      <div className='w-full max-w-3xl mx-auto text-center'>
-        <h1 className='text-3xl md:text-4xl font-bold tracking-tight mb-4'>
+    <section className="flex flex-1 flex-col items-center justify-center p-6 md:p-24">
+      <div className="mx-auto w-full max-w-3xl text-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight md:text-4xl">
           Shorten links in seconds with Short Linker
         </h1>
-        <p className='text-base text-muted-foreground mb-8 max-w-2xl mx-auto text-justify'>
+        <p className="text-muted-foreground mx-auto mb-8 max-w-2xl text-justify text-base">
           The Ultimate URL Shortening Platform with Smart AI Protection.
           Effortlessly shorten and manage your links while ensuring safety and
           reliability with advanced AI-powered URL flagging. Transform long URLs
