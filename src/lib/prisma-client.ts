1403faf77ee2e291aa7dc71bc4388f5cb5f9825d
srikanth <srikanth.thedev@gmail.com>
2025-03-24 14:43:12 +0000

    Add condition to render prisma log base on node env

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn', 'info']
-  });
+const prismaClientOptions: Prisma.PrismaClientOptions = {
+  log:
+    process.env.NODE_ENV === "development"
+      ? (["query", "error", "warn"] as Prisma.LogLevel[])
+      : (["error"] as Prisma.LogLevel[]),
+};
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export const prisma = global.prisma || new PrismaClient(prismaClientOptions);
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
